e078adeff66c095e029cb34747d2f9026a7d1e97
krittanut <krittanat12300@gmail.com>
2024-12-14 17:21:47 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ["bootstrap/dist/css/bootstrap.min.css"],
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",
+          type: "text/javascript",
+        },
+      ],
+    },
+  },
+});
---
+++ pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-primary">
+    test
+  </div>
+</template>
